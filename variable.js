fa2eff161b4cc18fb4b0681b1822ae138b1e64c0
jannat <devjannatbd@gmail.com>
2024-09-12 22:38:46 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,38 @@
+// // math need to know
+// // positive number-> 0,1,2,3,4,5,6.
+// // negative number -> -10, -20, -30.
+// // integer ->numaric->number -> [135736568]
+
+// //variable 
+// // vary + able = variable
+// var weight = 20;
+// console.log(weight);
+// var height = 20; //Number data type
+// var name = "jannat" ;//string data type
+// //boolen data type
+// var isOk = true;
+// bst areYouGoingUniversity = false;
+
+var totalAmmount =  500;
+var alu = 30;
+var potol = 50;
+var fruits = 300;
+var tommato = 80;
+var aluPotolTotal = alu + potol ;
+var fruitsTomatto = fruits + tommato ;
+
+var total = aluPotolTotal + fruitsTomatto;
+console.log(fruitsTomatto);
+console.log(aluPotolTotal);
+console.log(total);
+var reminder = totalAmmount - total;
+var multiply = alu + tommato;
+console.log(multiply);
+var division = alu / tommato;
+console.log(division);
+ var num1 = 10;
+ var num2 = 3;
+ console.log(num1/num2).toFixed((10));
+
+
+
